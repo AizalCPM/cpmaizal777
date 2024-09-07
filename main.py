16a39f152cafb620853667e8001075be3093df96
CPMAizal <cpmayyyan@gmail.com>
2024-09-07 14:39:31 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from rich.console import Console
 from rich.prompt import Prompt, IntPrompt
 from rich.text import Text
 from rich.style import Style
-from cpmnuker import CPMNuker
+from cpmaizal import CPMAizal
 
 __CHANNEL_USERNAME__ = "CPMAizal"
 __GROUP_USERNAME__   = "CPMAizalChat"
@@ -114,7 +114,7 @@ if __name__ == "__main__":
         acc_password = prompt_valid_value("[bold][?] Account Password[/bold]", "Password", password=False)
         acc_access_key = prompt_valid_value("[bold][?] Access Key[/bold]", "Access Key", password=False)
         console.print("[bold cyan][%] Trying to Login[/bold cyan]: ", end=None)
-        cpm = CPMNuker(acc_access_key)
+        cpm = CPMAizal(acc_access_key)
         login_response = cpm.login(acc_email, acc_password)
         if login_response != 0:
             if login_response == 100:
